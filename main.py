acccf5e7ed4769c5c4275af1cd92b0d1c6cbea3a
Pratul Singh <100825479+antonchigurh2@users.noreply.github.com>
2023-12-30 21:24:50 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,129 @@
+import argparse
+import os
+import pathlib
+import re
+import time
+import datetime
+
+import pandas as pd
+import torch
+from torch.utils.data import DataLoader
+
+from dataset import build_poisoned_training_set, build_testset, build_ood_testset
+from deeplearning import evaluate_badnets, optimizer_picker, train_one_epoch, evaluate_badnets_ood
+from models import BadNet
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torchvision.transforms as transforms
+    # import torch
+import pickle
+parser = argparse.ArgumentParser(description='Reproduce the basic backdoor attack in "Badnets: Identifying vulnerabilities in the machine learning model supply chain".')
+parser.add_argument('--dataset', default='CIFAR10', help='Which dataset to use (MNIST or CIFAR10, default: MNIST)')
+parser.add_argument('--model', default='resnet18', help='resnet18, vit, simple_conv')
+parser.add_argument('--nb_classes', default=10, type=int, help='number of the classification types')
+parser.add_argument('--load_local', action='store_true', help='train model or directly load model (default true, if you add this param, then load trained local model to evaluate the performance)')
+parser.add_argument('--loss', default='mse', help='Which loss function to use (mse or cross, default: mse)')
+parser.add_argument('--optimizer', default='sgd', help='Which optimizer to use (sgd or adam, default: sgd)')
+parser.add_argument('--epochs', type=int, default=100, help='Number of epochs to train backdoor model, default: 100')
+parser.add_argument('--batch_size', type=int, default=64, help='Batch size to split dataset, default: 64')
+parser.add_argument('--num_workers', type=int, default=0, help='Batch size to split dataset, default: 64')
+parser.add_argument('--lr', type=float, default=0.01, help='Learning rate of the model, default: 0.001')
+parser.add_argument('--download', action='store_true', help='Do you want to download data ( default false, if you add this param, then download)')
+parser.add_argument('--data_path', default='./data/', help='Place to load dataset (default: ./dataset/)')
+parser.add_argument('--device', default='cpu', help='device to use for training / testing (cpu, or cuda:1, default: cpu)')
+# poison settings
+parser.add_argument('--poisoning_rate', type=float, default=0.1, help='poisoning portion (float, range from 0 to 1, default: 0.1)')
+parser.add_argument('--trigger_label', type=int, default=1, help='The NO. of trigger label (int, range from 0 to 10, default: 0)')
+parser.add_argument('--trigger_path', default="./triggers/trigger_white.png", help='Trigger Path (default: ./triggers/trigger_white.png)')
+parser.add_argument('--trigger_size', type=int, default=35, help='Trigger Size (int, default: 5)')
+parser.add_argument('--print_step', type=int, default=2, help='')
+parser.add_argument('--clean_label', action='store_true', help='')
+parser.add_argument('--class_distinct_trigger', action='store_true', help='')
+parser.add_argument('--image_width', type=int, default=224, help='')
+parser.add_argument('--image_height', type=int, default=224, help='')
+
+args = parser.parse_args()
+
+
+# Assuming dataset_train is your PyTorch dataset
+# Replace 'output_directory' with the desired path where you want to save the dataset
+
+def main():
+    print("TESTING IF MAIN IS CHANGED OR NOT!!!")
+    print("{}".format(args).replace(', ', ',\n'))
+
+    if re.match('cuda:\d', args.device):
+        cuda_num = args.device.split(':')[1]
+        os.environ['CUDA_VISIBLE_DEVICES'] = cuda_num
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # if you're using MBP M1, you can also use "mps"
+
+    # create related path
+    pathlib.Path("./checkpoints/").mkdir(parents=True, exist_ok=True)
+    pathlib.Path("./logs/").mkdir(parents=True, exist_ok=True)
+
+    print("\n# load dataset: %s " % args.dataset)
+    dataset_train, args.nb_classes = build_poisoned_training_set(is_train=True, args=args)
+    dataset_val_clean, dataset_val_poisoned = build_testset(is_train=False, args=args)
+    dataset_val_clean_ood, dataset_val_poisoned_ood = build_ood_testset(is_train=False, args=args)
+
+    output_file = 'dataset_train.pth'
+    torch.save(dataset_train, output_file)
+
+    print(f'Dataset has been saved to {output_file}')
+    data_loader_val_clean_ood    = DataLoader(dataset_val_clean_ood,     batch_size=args.batch_size, shuffle=True, num_workers=2)
+    data_loader_val_poisoned_ood = DataLoader(dataset_val_poisoned_ood,  batch_size=args.batch_size,  shuffle=True, num_workers=2)
+    output_file='dataset_val_clean.pth'
+    torch.save(dataset_val_clean,output_file)
+    print(f'Dataset-Val-Clean has been saved to {output_file}')
+    output_file='dataset_val_poisoned.pth'
+    torch.save(dataset_val_poisoned,output_file)
+    print(f'Dataset-Val-poisoned has been saved to {output_file}')
+    data_loader_train        = DataLoader(dataset_train,         batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
+    data_loader_val_clean    = DataLoader(dataset_val_clean,     batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
+    data_loader_val_poisoned = DataLoader(dataset_val_poisoned,  batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers) # shuffle 随机化
+    model = BadNet(input_channels=dataset_train.channels, output_num=args.nb_classes, model=args.model).to(device)
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = optimizer_picker(args.optimizer, model.parameters(), lr=args.lr)
+    basic_model_path = "./checkpoints/vit-badnet-%s.pth" % args.dataset
+    torch.save(model.state_dict(), basic_model_path)
+
+    start_time = time.time()
+    if args.load_local:
+        print("## Load model from : %s" % basic_model_path)
+        model.load_state_dict(torch.load(basic_model_path), strict=True)
+        test_stats = evaluate_badnets(data_loader_val_clean, data_loader_val_poisoned, model, device)
+        ood_test_stats = evaluate_badnets_ood(data_loader_val_clean_ood, data_loader_val_poisoned_ood, model, device)
+        print(f"cifar10 classifer: Test Clean Accuracy(TCA): {test_stats['clean_acc']:.4f}")
+        print(f"cifar10 classifer: Attack Success Rate(ASR): {test_stats['asr']:.4f}")
+        print(f"OOD(cifar10 versus 100): Test Clean AUC(TCA): {ood_test_stats['clean_auc']:.4f} -- Test ADV AUC(ASR): {ood_test_stats['adv_auc']:.4f}")
+    else:
+        print(f"Start training for {args.epochs} epochs")
+        stats = []
+        for epoch in range(args.epochs):
+            torch.cuda.empty_cache()
+            train_stats = train_one_epoch(data_loader_train, model, criterion, optimizer, args.loss, device)
+            torch.cuda.empty_cache()
+            if epoch % args.print_step == 0:
+                test_stats = evaluate_badnets(data_loader_val_clean, data_loader_val_poisoned, model, device)
+                torch.cuda.empty_cache()
+                ood_test_stats = evaluate_badnets_ood(data_loader_val_clean_ood, data_loader_val_poisoned_ood, model, device)
+                print(f"# EPOCH {epoch}   loss: {train_stats['loss']:.4f} Test Acc: {test_stats['clean_acc']:.4f}, ASR: {test_stats['asr']:.4f}\n")
+                print(f"OOD(cifar10 versus 100): Test Clean AUC(TCA): {ood_test_stats['clean_auc']:.4f} -- Test ADV AUC(ASR): {ood_test_stats['adv_auc']:.4f}")
+                log_stats = {**{f'train_{k}': v for k, v in train_stats.items()},
+                                **{f'test_{k}': v for k, v in test_stats.items()},
+                                'epoch': epoch,
+                }
+                # save training stats
+                stats.append(log_stats)
+                df = pd.DataFrame(stats)
+                df.to_csv("./logs/%s_trigger%d.csv" % (args.dataset, args.trigger_label), index=False, encoding='utf-8')
+            # save model 
+            torch.save(model.state_dict(), basic_model_path)
+
+    total_time = time.time() - start_time
+    total_time_str = str(datetime.timedelta(seconds=int(total_time)))
+    print('Training time {}'.format(total_time_str))
+
+if __name__ == "__main__":
+    main()
---
+++ models/badnet.py
@@ -0,0 +1,86 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.models as models
+
+class Resent18_BadNet(torch.nn.Module):
+    def __init__(self, output_num=10, pretrained=True):
+        super().__init__()
+        # self.norm = lambda x: (x - mu) / std
+        self.backbone = models.resnet18(pretrained=pretrained)
+        self.backbone.fc = torch.nn.Identity()
+        self.output = torch.nn.Linear(512, output_num)
+
+    def forward(self, x):
+        # x = self.norm(x)
+        x = self.backbone(x)
+        # z_n = F.normalize(z1, dim=-1)
+        return self.output(x)
+
+class VIT_BadNet(torch.nn.Module):
+    def __init__(self, output_num=10, pretrained=True):
+        super().__init__()
+        # self.norm = lambda x: (x - mu) / std
+        self.backbone = models.vit_b_16(pretrained=pretrained)
+        self.backbone.heads = torch.nn.Identity()
+        self.output = torch.nn.Linear(768, output_num)
+        '''
+        num = 76
+        i = 0
+        for param in self.backbone.parameters():
+            if i<num:
+                param.requires_grad = False
+            i+=1
+        '''
+    def forward(self, x):
+        # x = self.norm(x)
+        x = self.backbone(x)
+        # z_n = F.normalize(z1, dim=-1)
+        return self.output(x)
+
+
+
+class Conv_BadNet(nn.Module):
+
+    def __init__(self, input_channels, output_num):
+        super().__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_channels, out_channels=16, kernel_size=5, stride=1),
+            nn.ReLU(),
+            nn.AvgPool2d(kernel_size=2, stride=2)
+        )
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, stride=1),
+            nn.ReLU(),
+            nn.AvgPool2d(kernel_size=2, stride=2)
+        )
+        fc1_input_features = 800 if input_channels == 3 else 512
+        self.fc1 = nn.Sequential(
+            nn.Linear(in_features=fc1_input_features, out_features=512),
+            nn.ReLU()
+        )
+        self.fc2 = nn.Sequential(
+            nn.Linear(in_features=512, out_features=output_num),
+            nn.Softmax(dim=-1)
+        )
+        self.dropout = nn.Dropout(p=.5)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+
+        x = x.view(x.size(0), -1)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        return x
+
+def BadNet(input_channels, output_num, model='resnet18'):
+    if model=='resnet18':
+        model = Resent18_BadNet(output_num=output_num)
+    elif model=='vit':
+        model = VIT_BadNet(output_num=output_num)
+    elif model=='simple_conv':
+        model = Conv_BadNet(input_channels=input_channels, output_num=output_num)
+    return model
+
